58e5f5b293d35aefb9c51f29726c942af51aa3ae
Paullo Bahia <paullotsw@gmail.com>
2026-02-01 03:37:27 -0300

    campaignSo script i missed last commit

---
+++ Assets/_MAIN/Scripts/Data/CampaignSO.cs
@@ -0,0 +1,23 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Gameplay.Core.Data
+{
+    [CreateAssetMenu(fileName = "Campaign", menuName = "Game/Campaign Data")]
+    public class CampaignSO : ScriptableObject
+    {
+        [Title("Campaign Config")]
+        [ListDrawerSettings(ShowIndexLabels = true)]
+        public List<LevelDataSO> Levels = new();
+
+        public LevelDataSO GetLevel(int index)
+        {
+            if (Levels == null || Levels.Count == 0) return null;
+            if (index < 0 || index >= Levels.Count) return null;
+
+            return Levels[index];
+        }
+    }
+}
