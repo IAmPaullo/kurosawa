ecf6c68d82da5c5b0bb451d64e3da8cbf8f5f69e
Paullo Bahia <paullotsw@gmail.com>
2026-02-03 17:30:24 -0300

    dissolved gameuicontroller god class into gameuipresenter and view classes

---
+++ Assets/_MAIN/Scripts/Controller/UI/GameUIPresenter.cs
@@ -0,0 +1,143 @@
+namespace Gameplay.UI
+{
+    using Gameplay.Core.Events;
+    using Gameplay.UI;
+    using Sirenix.OdinInspector;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class GameUIPresenter : MonoBehaviour
+    {
+        public enum UIState
+        {
+            PreMatch,
+            Playing,
+            Paused,
+            Ended
+        }
+
+        [SerializeField, Required] private GameUIView view;
+
+        [SerializeField] private List<GradientSO> availableThemes;
+        public GradientSO CurrentTheme { get; private set; }
+
+        private EventBinding<MatchStartEvent> matchStartBind;
+        private EventBinding<RequestPauseEvent> pauseRequestBind;
+        private EventBinding<MatchEndEvent> matchEndBind;
+
+        private UIState currentState;
+
+        private void Awake()
+        {
+            view.Initialize();
+            SetupButtonListeners();
+            SetState(UIState.PreMatch, false);
+        }
+
+        private void OnEnable()
+        {
+            pauseRequestBind = new EventBinding<RequestPauseEvent>(OnPauseRequest);
+            matchEndBind = new EventBinding<MatchEndEvent>(OnMatchEnd);
+            matchStartBind = new EventBinding<MatchStartEvent>(OnMatchStart);
+
+            EventBus<RequestPauseEvent>.Register(pauseRequestBind);
+            EventBus<MatchEndEvent>.Register(matchEndBind);
+            EventBus<MatchStartEvent>.Register(matchStartBind);
+        }
+
+        private void OnDisable()
+        {
+            EventBus<RequestPauseEvent>.Deregister(pauseRequestBind);
+            EventBus<MatchEndEvent>.Deregister(matchEndBind);
+            EventBus<MatchStartEvent>.Deregister(matchStartBind);
+        }
+
+        private void SetupButtonListeners()
+        {
+            if (view.ReturnToGameButton)
+            {
+                view.ReturnToGameButton.onClick.RemoveAllListeners();
+                view.ReturnToGameButton.onClick.AddListener(OnResumeClicked);
+            }
+
+            if (view.PauseButton)
+            {
+                view.PauseButton.onClick.RemoveAllListeners();
+                view.PauseButton.onClick.AddListener(() => EventBus<RequestPauseEvent>.Raise(new RequestPauseEvent()));
+            }
+
+            if (view.PlayButton)
+            {
+                view.PlayButton.onClick.RemoveAllListeners();
+                view.PlayButton.onClick.AddListener(OnPlayClicked);
+            }
+
+            if (view.NextLevelButton)
+            {
+                view.NextLevelButton.onClick.RemoveAllListeners();
+                view.NextLevelButton.onClick.AddListener(() => EventBus<RequestNextLevelEvent>.Raise(new RequestNextLevelEvent()));
+            }
+
+            if (view.BackToMenuButton)
+            {
+                view.BackToMenuButton.onClick.RemoveAllListeners();
+                view.BackToMenuButton.onClick.AddListener(() =>
+                {
+                    if (SceneEvents.Instance != null)
+                        SceneEvents.Instance.LoadMainMenuAsync();
+                });
+            }
+
+            if (view.MeetMeButton)
+            {
+                view.MeetMeButton.onClick.RemoveAllListeners();
+                view.MeetMeButton.onClick.AddListener(() => view.OpenMeetMeWindow());
+            }
+
+            if (view.ThemesButton)
+            {
+                view.ThemesButton.onClick.RemoveAllListeners();
+                view.ThemesButton.onClick.AddListener(() => view.OpenThemesWindow());
+            }
+        }
+
+        private void OnPlayClicked()
+        {
+            EventBus<RequestMatchStartEvent>.Raise(new RequestMatchStartEvent());
+            SetState(UIState.Playing, false);
+        }
+
+        private void OnResumeClicked()
+        {
+            EventBus<RequestResumeEvent>.Raise(new RequestResumeEvent());
+            SetState(UIState.Playing, true);
+        }
+
+        private void OnMatchStart(MatchStartEvent evt)
+        {
+            SetState(UIState.Playing, false);
+        }
+
+        private void OnPauseRequest(RequestPauseEvent evt)
+        {
+            SetState(UIState.Paused, true);
+        }
+
+        private void OnMatchEnd(MatchEndEvent evt)
+        {
+            SetState(UIState.Ended, true);
+        }
+
+        public void ApplyTheme(GradientSO theme)
+        {
+            CurrentTheme = theme;
+            EventBus<ThemeUpdateEvent>.Raise(new ThemeUpdateEvent { Theme = CurrentTheme });
+        }
+
+        private void SetState(UIState state, bool animate)
+        {
+            currentState = state;
+            view.UpdateStateVisuals(state, animate);
+        }
+    }
+}
---
+++ Assets/_MAIN/Scripts/Controller/UI/GameUIView.cs
@@ -0,0 +1,167 @@
+using DG.Tweening;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Gameplay.UI
+{
+    public class GameUIView : MonoBehaviour
+    {
+        [Header("Canvas & Background")]
+        [SerializeField] private Canvas overlayUICanvas;
+        [SerializeField] private Image background;
+        [SerializeField] private CanvasGroup canvasGroup;
+
+        [Header("Layout Groups")]
+        [SerializeField] private RectTransform topSideGroup;
+        [SerializeField] private RectTransform rightSideGroup;
+        [SerializeField] private RectTransform bottomSideGroup;
+
+        [Header("Windows")]
+        [SerializeField] private UIWindowController themesWindow;
+        [SerializeField] private UIWindowController meetMeWindow;
+
+        [Header("Buttons")]
+        [SerializeField] public Button PlayButton;
+        [SerializeField] public Button SettingsButton;
+        [SerializeField] public Button ReturnToGameButton;
+        [SerializeField] public Button PauseButton;
+        [SerializeField] public Button RewardsButtons;
+        [SerializeField] public Button MeetMeButton;
+        [SerializeField] public Button ThemesButton;
+        [SerializeField] public Button NextLevelButton;
+        [SerializeField] public Button BackToMenuButton;
+
+        [Header("End Game")]
+        [SerializeField] private GameObject endGameContainer;
+
+        [Header("Animation Settings")]
+        [SerializeField] private float moveOffset = 500f;
+        [SerializeField] private float animDuration = 0.5f;
+        [SerializeField] private Ease moveInEase = Ease.Linear;
+        [SerializeField] private Ease moveOutEase = Ease.Linear;
+
+        private Vector2 topTargetPos;
+        private Vector2 rightTargetPos;
+        private Vector2 bottomTargetPos;
+        private Sequence uiSequence;
+
+
+        public GameObject PlayButtonObj => PlayButton ? PlayButton.gameObject : null;
+        public GameObject PauseButtonObj => PauseButton ? PauseButton.gameObject : null;
+        public GameObject ReturnToGameButtonObj => ReturnToGameButton ? ReturnToGameButton.gameObject : null;
+        public GameObject EndGameContainerObj => endGameContainer;
+
+        public void Initialize()
+        {
+            // Salva posições iniciais
+            topTargetPos = GetAnchoredPos(topSideGroup);
+            rightTargetPos = GetAnchoredPos(rightSideGroup);
+            bottomTargetPos = GetAnchoredPos(bottomSideGroup);
+
+            // Define posições iniciais (fora da tela ou onde o layout mandar)
+            SetAnchoredPos(topSideGroup, topTargetPos + (Vector2.up * moveOffset));
+            SetAnchoredPos(rightSideGroup, rightTargetPos + (Vector2.right * moveOffset));
+            SetAnchoredPos(bottomSideGroup, bottomTargetPos + (Vector2.down * moveOffset));
+        }
+
+        public void UpdateStateVisuals(GameUIPresenter.UIState state, bool animate)
+        {
+            bool showPlay = state == GameUIPresenter.UIState.PreMatch;
+            bool showPause = state == GameUIPresenter.UIState.Playing;
+            bool showResume = state == GameUIPresenter.UIState.Paused;
+            bool showEndGame = state == GameUIPresenter.UIState.Ended;
+
+            if (PlayButtonObj) PlayButtonObj.SetActive(showPlay);
+            if (PauseButtonObj) PauseButtonObj.SetActive(showPause);
+            if (ReturnToGameButtonObj) ReturnToGameButtonObj.SetActive(showResume);
+            if (EndGameContainerObj) EndGameContainerObj.SetActive(showEndGame);
+
+            // Logica original: Se não estiver pausado, fecha janelas
+            if (state != GameUIPresenter.UIState.Paused)
+            {
+                CloseAllWindows();
+            }
+
+            if (animate)
+            {
+                if (state == GameUIPresenter.UIState.Playing || state == GameUIPresenter.UIState.PreMatch)
+                    AnimateOut();
+                else
+                    AnimateIn(); // Paused ou Ended anima o menu de volta
+            }
+            else
+            {
+                if (state == GameUIPresenter.UIState.Playing || state == GameUIPresenter.UIState.PreMatch)
+                    SetMenuHidden();
+                else
+                    SetMenuVisible();
+            }
+        }
+
+        public void OpenThemesWindow() => themesWindow?.Open();
+        public void OpenMeetMeWindow() => meetMeWindow?.Open();
+
+        private void CloseAllWindows()
+        {
+            themesWindow?.Close();
+            meetMeWindow?.Close();
+        }
+
+        // --- Animation Logic (Mantida Original) ---
+
+        private void AnimateIn()
+        {
+            uiSequence?.Kill();
+            uiSequence = DOTween.Sequence().SetUpdate(true);
+            canvasGroup.interactable = true;
+
+            uiSequence.Append(background.DOFade(.25f, .25f));
+            uiSequence.Append(MoveContainer(topSideGroup, topTargetPos, animDuration, moveInEase));
+            uiSequence.Join(MoveContainer(rightSideGroup, rightTargetPos, animDuration, moveInEase)); // Usei Join pra ficar simultaneo como esperado, mas ajustei pro original se necessario
+            uiSequence.Join(MoveContainer(bottomSideGroup, bottomTargetPos, animDuration, moveInEase));
+        }
+
+        private void AnimateOut()
+        {
+            uiSequence?.Kill();
+            uiSequence = DOTween.Sequence().SetUpdate(true);
+            canvasGroup.interactable = false;
+
+            uiSequence.Join(background.DOFade(0, .25f));
+            uiSequence.Join(MoveContainer(topSideGroup, topTargetPos + (Vector2.up * moveOffset), animDuration, moveOutEase));
+            uiSequence.Join(MoveContainer(rightSideGroup, rightTargetPos + (Vector2.right * moveOffset), animDuration, moveOutEase));
+            uiSequence.Join(MoveContainer(bottomSideGroup, bottomTargetPos + (2 * moveOffset * Vector2.down), animDuration, moveOutEase));
+        }
+
+        private void SetMenuVisible()
+        {
+            uiSequence?.Kill();
+            canvasGroup.interactable = true;
+            background.color = new Color(background.color.r, background.color.g, background.color.b, .25f);
+            SetAnchoredPos(topSideGroup, topTargetPos);
+            SetAnchoredPos(rightSideGroup, rightTargetPos);
+            SetAnchoredPos(bottomSideGroup, bottomTargetPos);
+        }
+
+        private void SetMenuHidden()
+        {
+            uiSequence?.Kill();
+            canvasGroup.interactable = false;
+            background.color = new Color(background.color.r, background.color.g, background.color.b, 0f);
+            SetAnchoredPos(topSideGroup, topTargetPos + (Vector2.up * moveOffset));
+            SetAnchoredPos(rightSideGroup, rightTargetPos + (Vector2.right * moveOffset));
+            SetAnchoredPos(bottomSideGroup, bottomTargetPos + (Vector2.down * moveOffset));
+        }
+
+        private Tween MoveContainer(Transform target, Vector2 targetPos, float duration, Ease ease)
+        {
+            RectTransform rt = target.GetComponent<RectTransform>();
+            return rt.DOAnchorPos(targetPos, duration).SetEase(ease);
+        }
+
+        private Vector2 GetAnchoredPos(Transform t) => t.GetComponent<RectTransform>().anchoredPosition;
+        private void SetAnchoredPos(Transform t, Vector2 pos) => t.GetComponent<RectTransform>().anchoredPosition = pos;
+    }
+}
