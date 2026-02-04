b52204ed5f1c3caa19be3c7c774f903b1426f6f8
Paullo Bahia <paullotsw@gmail.com>
2026-02-04 19:34:38 -0300

    updated all ui elements on game scene to use themecolorapplier and playsoundonclick

---
+++ Assets/_MAIN/Scripts/Controller/UI/PlaySoundOnClick.cs
@@ -0,0 +1,40 @@
+using Gameplay.Audio;
+using Sirenix.OdinInspector;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Gameplay.Audio
+{
+    [RequireComponent(typeof(Button))]
+    public class PlaySoundOnClick : MonoBehaviour
+    {
+        [SerializeField] private SFXType soundType = SFXType.UI_Click;
+        [SerializeField, Required] private Button button;
+
+
+        void Reset()
+        {
+            TryGetComponent(out button);
+        }
+
+        void Awake()
+        {
+            if (button == null)
+                TryGetComponent(out button);
+        }
+        private void Start()
+        {
+            if (button == null)
+                Debug.LogError("Button component is missing");
+
+            button.onClick.AddListener(() =>
+             {
+                 if (AudioManager.Instance != null)
+                 {
+                     AudioManager.Instance.PlaySFX(soundType);
+                 }
+             });
+        }
+    }
+}
